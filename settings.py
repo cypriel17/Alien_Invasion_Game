25a8ea76b745396490f312d4e40c2ef7b49324c4
Zwelithini <cypriel17@gmail.com>
2024-04-02 19:38:00 +0200

    firing bullets from ships, and updating tests. In addition, there's commented test I'd see it on the next iteration.

--- settings.py
+++ settings.py
@@ -7,9 +7,16 @@ class Settings:
         """Intialize game's settings."""
 
         #screen settings
-        self.screen_width = 1600
-        self.screen_height = 900
+        self.screen_width = 1200
+        self.screen_height = 800
         self.bg_color = (255, 255, 255)
 
         #ship settings
         self.ship_speed = 1.5
+
+        #bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (255, 0, 0)
+        self.bullets_allowed = 3
