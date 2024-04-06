4805214c59fcff579dcf34fe8111cb08fbbf2ccd
Zwelithini <cypriel17@gmail.com>
2024-04-06 13:27:34 +0200

    alien movements done, and tests updated. However not all, it's hard testing testing something updating precisely every second of the runtime - but I'l find a way

--- alien.py
+++ alien.py
@@ -9,9 +9,10 @@ class Alien(Sprite):
     
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         
         # Load the alien image and set its rect attribute
-        self.image = pygame.image.load('images/enemy_ship_1.jpeg')
+        self.image = pygame.image.load('images/enemy_ship.bmp')
         self.rect = self.image.get_rect()
         
         # start each new alien near the top left of the screen
@@ -20,3 +21,16 @@ class Alien(Sprite):
         
         # store the alien's exact horizontal position
         self.x = float(self.rect.x)
+
+    def update(self):
+        """Move the alien to the specified position"""
+        self.x += (self.settings.alien_speed * self.settings.fleet_direction)
+        self.rect.x = self.x
+
+    
+    def check_edges(self):
+        """Returns True if is at edge"""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
+        return False
--- settings.py
+++ settings.py
@@ -20,3 +20,9 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (255, 0, 0)
         self.bullets_allowed = 3
+
+        # Alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1 # right rep by 1; left rep by -1
+        
