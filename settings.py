d2ae1a30839b8f1276bb20009a81aa36f216a54a
cypriel17 <cypriel17@gmail.com>
2024-05-11 19:45:21 +0200

    leveling up the speed of the as it progresses

--- settings.py
+++ settings.py
@@ -12,20 +12,16 @@ class Settings:
         self.bg_color = (255, 255, 255)
 
         #ship settings
-        self.ship_speed = 1.5
         self.ship_limit = 3
 
         #bullet settings
-        # self.bullet_speed = 1.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (255, 0, 0)
         self.bullets_allowed = 3
 
         # Alien settings
-        self.alien_speed = 1.0
         self.fleet_drop_speed = 10
-        self.fleet_direction = 1 # right rep by 1; left rep by -1
         
         # How quickly the game speeds up
         self.speedup_scale = 1.1
